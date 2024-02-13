2b053849758fa781143a86e013fef63d6e4c8f5a
Gabriel Pichot <gabriel.pichot@gmail.com>
2024-02-13 14:27:40 +0100

    feat: add routing

--- pokeapp/src/App.tsx
+++ pokeapp/src/App.tsx
@@ -2,20 +2,64 @@ import "./App.css";
 import { pokemons } from "./mocks";
 import PokemonList from "./components/PokemonList";
 import PokemonCard from "./components/PokemonCard";
-import React from "react";
 import PokemonForm from "./components/PokemonForm";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
-function App() {
+// PokemonListPage
+// PokemonFormPage
+
+function Menu() {
+  return (
+    <nav style={{ display: "flex", gap: "1rem" }}>
+      <a href="/">Accueil</a>
+      <a href="/new">Nouveau pokemon</a>
+      <a href="/about">A propos</a>
+    </nav>
+  );
+}
+
+function PokemonListPage() {
+  return (
+    <PokemonList>
+      {pokemons.map((pokemon) => (
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+      ))}
+    </PokemonList>
+  );
+}
+
+function Root() {
   return (
     <>
-      <PokemonForm />
-      <PokemonList>
-        {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </PokemonList>
+      <Menu />
+      <Outlet />
     </>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <PokemonListPage />,
+      },
+      {
+        path: "/new",
+        element: <PokemonForm />,
+      },
+      {
+        path: "/about",
+        element: <h1>A propos</h1>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- pokeapp/src/components/PokemonCard.tsx
+++ pokeapp/src/components/PokemonCard.tsx
@@ -3,15 +3,13 @@ import styles from "./PokemonCard.module.css";
 
 interface PokemonCardProps {
   pokemon: PokemonDetail;
-  onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
 export default function PokemonCard(props: PokemonCardProps) {
-  const { onClick } = props;
   const { image, name, types } = props.pokemon;
 
   return (
-    <div className={styles.pokemonCard} onClick={onClick}>
+    <div className={styles.pokemonCard}>
       <img src={image} alt={name} />
       <h2 className={styles.name}>{name}</h2>
       <p>{types.join(", ")}</p>
